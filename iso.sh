f1b494d09c9170ec89f40da4967560ce6b7fbbe5
loic <loic@WhiteWolf>
2024-02-17 00:39:16 +0100

    meaty skeleton base

---
+++ iso.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/${KERNEL_NAME}.kernel isodir/boot/${KERNEL_NAME}.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "${KERNEL_NAME}" {
+	multiboot /boot/${KERNEL_NAME}.kernel
+}
+EOF
+grub-mkrescue /usr/lib/grub/i386-pc -o ${KERNEL_NAME}.iso isodir
